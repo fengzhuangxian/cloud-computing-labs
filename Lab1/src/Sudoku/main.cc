c800a8799bc2ec2a5a3ff86fcdb8dd39ee281ace
isxqian <isxqian@DESKTOP-CB16R2O>
2025-04-05 09:53:05 +0800

    read from stdInput

--- Lab1/src/Sudoku/main.cc
+++ Lab1/src/Sudoku/main.cc
@@ -15,30 +15,26 @@ int64_t now()
 
 int main(int argc, char* argv[])
 {
-  init_neighbors();
+    char testfile[128]= "\0";
+    while(scanf("%s",testfile)){//接收文件路径输入
+        char puzzle[128];
 
-  FILE* fp = fopen(argv[1], "r");
-  char puzzle[128];
-  int total_solved = 0;
-  int total = 0;
-  bool (*solve)(int) = solve_sudoku_basic;
-  if (argv[2] != NULL)
-    if (argv[2][0] == 'a')
-      solve = solve_sudoku_min_arity;
-    else if (argv[2][0] == 'c')
-      solve = solve_sudoku_min_arity_cache;
-    else if (argv[2][0] == 'd')
-      solve = solve_sudoku_dancing_links;
-  int64_t start = now();
-  while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
+    FILE* f1 = fopen(testfile, "r");
+    if (!f1) {
+        perror("无法打开文件");
+        continue;  // 跳过错误文件
+    }
+    int total_solved = 0;
+    int total = 0;
+    bool (*solve)(int) = solve_sudoku_dancing_links;
+    int64_t start = now();
+    while (fgets(puzzle, sizeof puzzle, f1) != NULL) {
     if (strlen(puzzle) >= N) {
       ++total;
       input(puzzle);
       init_cache();
-      //if (solve_sudoku_min_arity_cache(0)) {
-      //if (solve_sudoku_min_arity(0))
-      //if (solve_sudoku_basic(0)) {
       if (solve(0)) {
+        printf("%s\n",puzzle);
         ++total_solved;
         if (!solved())
           assert(0);
@@ -51,6 +47,7 @@ int main(int argc, char* argv[])
   int64_t end = now();
   double sec = (end-start)/1000000.0;
   printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
+}
 
   return 0;
 }
